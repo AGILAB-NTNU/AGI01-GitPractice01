f540dc2297f8424e3a5018e1ea3f77194a7b0f42
Angus9312 <angus932011@gmail.com>
2026-07-06 15:41:01 +0800

    feat: 新增MNIST訓練與測試資料的下載及顯示第一張圖片

---
+++ notebooks/notebook_mnist_training.py
@@ -0,0 +1,23 @@
+#%%
+
+import torch
+from torch import nn
+import torchvision as TV
+from IPython.display import clear_output
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+train_data = TV.datasets.MNIST("../dataset/MNIST/", train=True, transform=None,target_transform=None,download=True) #下載並匯入MNIST訓練資料
+test_data = TV.datasets.MNIST("../dataset/MNIST/", train=False, transform=None,target_transform=None,download=True) #下載並匯入MNIST測試資料
+
+print('Number of samples in train_data is: ',len(train_data))
+print('Number of samples in test_data is: ',len(test_data))
+# %%
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+x = train_data.data[0]
+plt.imshow(x)
+# %%
