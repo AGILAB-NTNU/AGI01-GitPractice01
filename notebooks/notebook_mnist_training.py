8b1651dd05bbb1c1fcf47d5d1cfe70b5f9cdd830
Angus9312 <angus932011@gmail.com>
2026-07-06 15:47:24 +0800

    fix: 修正torchvision匯入名稱以符合慣例

--- notebooks/notebook_mnist_training.py
+++ notebooks/notebook_mnist_training.py
@@ -2,14 +2,14 @@
 
 import torch
 from torch import nn
-import torchvision as TV
+import torchvision as tv
 from IPython.display import clear_output
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-train_data = TV.datasets.MNIST("../dataset/MNIST/", train=True, transform=None,target_transform=None,download=True) #下載並匯入MNIST訓練資料
-test_data = TV.datasets.MNIST("../dataset/MNIST/", train=False, transform=None,target_transform=None,download=True) #下載並匯入MNIST測試資料
+train_data = tv.datasets.MNIST("../dataset/MNIST/", train=True, transform=None,target_transform=None,download=True) #下載並匯入MNIST訓練資料
+test_data = tv.datasets.MNIST("../dataset/MNIST/", train=False, transform=None,target_transform=None,download=True) #下載並匯入MNIST測試資料
 
 print('Number of samples in train_data is: ',len(train_data))
 print('Number of samples in test_data is: ',len(test_data))
